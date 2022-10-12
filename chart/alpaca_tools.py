e20024fd3a2963b59bc41de565d353c03a4b81d3
Frank Albanese <albanesefc9@gmail.com>
2022-10-12 07:12:50 -0400

    Faster

--- chart/alpaca_tools.py
+++ chart/alpaca_tools.py
@@ -4,6 +4,7 @@ from alpaca_trade_api.rest import TimeFrame
 import datetime
 from dateutil.relativedelta import relativedelta
 import pandas as pd
+import time
 
 api = alpaca.REST(ALPACA_KEY, ALPACA_SECRET, ALPACA_URL)
 last_year = (datetime.datetime.now() - relativedelta(years=1)).strftime('%Y-%m-%d')
@@ -12,6 +13,7 @@ account = api.get_account()
 buying_power = float(account.buying_power)
 
 def get_df(ticker):
+    time.sleep(.5)
     df = api.get_bars(ticker, TimeFrame.Day, last_year, yesterday, adjustment='raw').df
     df['datetime'] = pd.to_datetime(df.index, unit='ms').strftime('%m/%d/%Y')
     return df
--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+from asyncore import write
 from http.client import RemoteDisconnected
 from xmlrpc.client import ResponseError
 import chart
@@ -7,8 +8,10 @@ import time
 from chart.trade import buy
 from chart.alpaca_tools import buying_power
 from chart.send_msg import send_error
+import csv
 
 def run():
+    rows = []
     x = 0
     fail_count = 0
     success_count = 0
@@ -19,16 +22,18 @@ def run():
     try:
         for n in t:
             try:
+                time.sleep(.5)
                 obj_data = chart.Equity(n)
                 print(obj_data)
                 success_count += 1
                 x += 1
-                size = (obj_data.position_size * buying_power) / obj_data.price
-                buy_qty = int(round(size, 0))
-                if obj_data.is_buy == True:
-                    buy(obj_data.name, buy_qty)
+                rows.append([obj_data.name, obj_data.price, obj_data.chart['MA10'][-1], obj_data.chart['MA20'][-1], obj_data.chart['Volume20MA'][-1], obj_data.chart['RSI'][-1], obj_data.chart['MACD'][-1], obj_data.chart['HV'][-1], obj_data.is_buy])
+                # size = (obj_data.position_size * buying_power) / obj_data.price
+                # buy_qty = int(round(size, 0))
+                # if obj_data.is_buy == True:
+                #     buy(obj_data.name, buy_qty)
                 if success_count % 1000 == 0:
-                    time.sleep(60)
+                    time.sleep(240)
                 elif success_count % 50 == 0:
                     time.sleep(30)
                 
@@ -36,9 +41,10 @@ def run():
                 print( f'{n}: Unable to load data')
                 print(f'{error}\n')
                 send_error(error)
-                fail_count += 1
-                errors_json[f'{n}'] = f'{traceback.extract_tb(error.__traceback__)}', f'{error}'
-                ameritrade_ticker_fails = [n for n in errors_json.keys()]
+                with open('scan.csv', 'w') as file:
+                    writer = csv.writer(file)
+                    for n in rows:
+                        writer.writerow(n)
             if fail_count > 0:
                 print(f'Success: {success_count} / Fails: {fail_count}\nCapture Rate: {round(((success_count / (success_count + fail_count)) * 100), 3)}%')
             else:
@@ -47,6 +53,10 @@ def run():
         print('Updating Bad Ticker List before Executing')
 
     print(f'Fails: {fail_count}\nSuccess: {success_count}\nOut of: {len(t)} requests\nCapture Rate: {round(((success_count / (success_count + fail_count)) * 100), 3)}%')
+    with open('scan.csv', 'w') as file:
+        writer = csv.writer(file)
+        for n in rows:
+            writer.writerow(n)
 
 
 if __name__ == '__main__':
